ba5ceca85b91f50d9c41f6e687a6b6d3770a9314
loshkasaginov <84158585+loshkasaginov@users.noreply.github.com>
2023-04-30 20:56:47 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+DATABASE_URL = "postgresql://postgres:061203-13a@localhost/tgd"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    create_tables()
